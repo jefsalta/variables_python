22c1fe9fad5e1ba2d0317e0dbe3437a26a6786d3
jefsalta <101189940+jefsalta@users.noreply.github.com>
2022-03-17 12:38:18 -0300

    Add files via upload

--- ejercicios_practica/ejercicio_1.py
+++ ejercicios_practica/ejercicio_1.py
@@ -17,8 +17,12 @@ numero_2 = 7
 # Almacenar el valor de la suma en una variable
 # ej:
 # operacion = .....
-
+resultado= numero_1 + numero_2
 # Imprimir en pantalla el resultado de la suma
 # print(....)
-
+print("Sumar",numero_1,"y",numero_2)
+print(numero_1,"+", numero_2,"=", resultado)
 # Repita el procedimiento para realizar la resta
+resultado= numero_1 - numero_2
+print("Restar",numero_1,"y",numero_2)
+print(numero_1,"-",numero_2,"=",resultado)
--- ejercicios_practica/ejercicio_2.py
+++ ejercicios_practica/ejercicio_2.py
@@ -19,7 +19,8 @@ numero_2 = int(input())
 
 # Alumno: Imprima en pantalla los dos números enteros solicitados
 # print(....)
-
+print("Número 1:",numero_1)
+print("Número 2:",numero_2)
 # Alumno: Calcule la suma, resta, división y multiplicación de los números ingresados
 # numero_1, numero_2
 # Imprima en pantalla todos los resultados con el siguiente formato de ejemplo:
@@ -27,9 +28,10 @@ numero_2 = int(input())
 # NOTA: No coloque usted los nùmeros y resultados, use las variables
 
 # Suma
-
+print("El resultado de sumar",numero_1,"y",numero_2,"es",numero_1 + numero_2)
 # Resta
-
+print("El resultado de restar",numero_1,"y",numero_2,"es",numero_1 - numero_2)
 # División
-
+print("El resultado de dividir",numero_1,"entre",numero_2,"es",numero_1 / numero_2)
 # Multiplicación
+print("El resultado de multiplicar",numero_1,"por",numero_2,"es",numero_1 * numero_2)
